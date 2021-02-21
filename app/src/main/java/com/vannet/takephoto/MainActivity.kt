9f9dab66bc64adbf4655a478cea9450c3486cc49
Veng Vannet <vannetvengpp@gmail.com>
2021-02-21 12:28:23 +0700

    open the camera to take the photo

--- app/src/main/java/com/vannet/takephoto/MainActivity.kt
+++ app/src/main/java/com/vannet/takephoto/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.vannet.takephoto
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    val REQUEST_IMAGE_CAPTURE = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun openCamera(view: View) {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE)
+        }catch (e:ActivityNotFoundException){
+            Toast.makeText(applicationContext,"Not Found",Toast.LENGTH_SHORT).show()
+        }
     }
 }
